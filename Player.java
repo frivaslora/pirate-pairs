8317e465101b1c67ff65a564c586d7714b1d2998
Fernando Rivas <frivaslora@Brooksschool.org>
2026-02-11 09:56:11 -0500

    Player1

---
+++ Player.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Player {
+    private int id;
+    private int score = 0;
+    private int [] hand = new int[20];
+    private int handCount = 0;
+    private boolean eliminated = false;
+
+    public Player(int id){
+        this.id = id
+    }
+    
+}
